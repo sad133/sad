1b1f803faf5f07d449e4d4b44c5ff12da640b634
sad133 <46084990+sad133@users.noreply.github.com>
2018-12-23 17:39:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -105,25 +105,6 @@ client.on("message", (message) => {
     }
 });
 
-  client.on('message', message => {
-    if (!message.channel.guild) return;
-    if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply("***  ليس معك صلاحيات  ***")
-    var prefix = "$";
-       if (message.content ===   prefix + "add role") {
-    message.channel.send("<@" + message.author.id + ">  ***  جاري اعطاء الرتبة للاعضاء كما امرت  *** ")
-    message.guild.members.forEach(m => {
-    m.addRole(message.guild.roles.find('name', 'اسم الرتبة'))
-    })
-    }
-    
-       if (message.content ===   prefix + "remove role") {
-    message.channel.send("<@" + message.author.id + ">  ***  جاري اعطاء الرتبة للاعضاء كما امرت  *** ")
-    message.guild.members.forEach(m => {
-    m.removeRole(message.guild.roles.find('name', 'اسم الرتبة'))
-    })
-    }
-    
-    });
 	client.on('message', message => {
     if(!message.channel.guild) return;
 if (message.content.startsWith('$ping')) {
